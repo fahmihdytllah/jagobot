34aa4859d0878c12a8e66178c0c2bf3559e48d2e
Fahmi Hidayatulloh <fahmicog@gmail.com>
2024-06-18 00:05:18 +0700

    new

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 # Author    : https://www.instagram.com/fahmihdytllah
 # Portfolio : https://fahmihdytllah.me
 # Website   : https://bot.jagocode.my.id
-# Updated   : 6 May 2024 11:14
+# Updated   : 17 jun 2024 23:18
 
-# Note : Make sure you have changed the token in the config.json file, so that no errors occur when running the script with pm2
 
 if command -v node &> /dev/null; then
     node_version=$(node -v)
@@ -18,18 +17,15 @@ else
 fi
 
 echo "[✓] Installing components..."
-npm install
 if ! dpkg -s libgtk1.0-0 >/dev/null 2>&1; then
-    sudo apt-get install libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 -y
+    sudo apt-get install git libx11-xcb1 libxcomposite1 libasound2 libatk1.0-0 libatk-bridge2.0-0 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 -y
 fi
 
-if ! command -v pm2 &> /dev/null
-then
-    echo "[✓] PM2 not found. Installing..."
-    npm install -g pm2
-fi
-
-echo "[✓] Starting bot..."
-pm2 start bot.js
+npm install
+npm install -g pm2
 
 xdg-open https://youtube.com/@JagoCode
+
+echo "[✓] All components have been installed..."
+echo "[✓] Run normally > node bot.js"
+echo "[✓] Run in the background > pm2 start bot.js"
---
+++ update.sh
@@ -0,0 +1,22 @@
+# Author    : https://www.instagram.com/fahmihdytllah
+# Portfolio : https://fahmihdytllah.me
+# Website   : https://bot.jagocode.my.id
+# Updated   : 17 jun 2024 23:28
+
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if command_exists git; then
+  echo "[✓] Updating to the latest version..."
+  git pull origin main
+  
+  echo "[✓] Updating dependencies..."
+  npm install
+else
+  echo "[!] Git not installed..."
+fi
+
+echo "[✓] All components have been successfully updated..."
+echo "[✓] Run normally > node bot.js"
+echo "[✓] Run in the background > pm2 start bot.js"
